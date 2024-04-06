744e4f7b80d12f3e8e52bb65cf1b05a677f8c9b4
qadis1dev <alqadis1devops@gmail.com>
2024-04-06 15:37:37 +0400

    logout mockup

--- lib/screens/logged.dart
+++ lib/screens/logged.dart
@@ -1,3 +1,7 @@
+// ignore_for_file: use_build_context_synchronously
+
+import 'package:app/root.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class Logged extends StatefulWidget {
@@ -11,7 +15,22 @@ class _LoggedState extends State<Logged> {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      
+      child: Column(
+        children: [
+          SizedBox(height: 50,),
+          Text("Logged"),
+          ElevatedButton(
+            onPressed: () async {
+              await FirebaseAuth.instance.signOut();
+              Navigator.of(context).pushAndRemoveUntil(
+                MaterialPageRoute(builder: (context) => Root()),
+                (route) => false
+              );
+            },
+            child: Text("Signout")
+          )
+        ],
+      ),
     );
   }
 }
