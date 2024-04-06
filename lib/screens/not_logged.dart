49f0cb8393ea61fc8f1e182d5cf6f2a817a1962d
qadis1dev <alqadis1devops@gmail.com>
2024-04-06 23:13:37 +0400

    v

--- lib/screens/not_logged.dart
+++ lib/screens/not_logged.dart
@@ -4,7 +4,7 @@ import 'package:app/root.dart';
 import 'package:app/screens/register.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:google_sign_in/google_sign_in.dart';
+// import 'package:google_sign_in/google_sign_in.dart';
 import "package:firebase_auth/firebase_auth.dart";
 
 class NotLogged extends StatefulWidget {
